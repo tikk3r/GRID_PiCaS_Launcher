ce1d74e81ebb90388c0f08bd95d49765621f96d9
Alexandar Mechev <apmechev@softdrive.grid.sara.nl>
2016-12-07 16:39:54 +0100

    Added docstrings in Token

--- Token.py
+++ Token.py
@@ -1,40 +1,59 @@
 import sys
 import os
 import ConfigParser
-
-if not 'couchdb' in sys.modules:
-    import couchdb
-from couchdb.design import ViewDefinition
-
 import pdb
 import itertools
 
+if 'couchdb' not in sys.modules:
+    import couchdb
+from couchdb.design import ViewDefinition
+
+
+__author__ = "Alexandar P. Mechev"
+__copyright__ = ""
+__credits__ = ["Alexandar P. Mechev", "Natalie Danezi"]
+__license__ = "GPL"
+__version__ = "3.0.0"
+__maintainer__ = "Alexandar P. Mechev"
+__email__ = "apmechev@strw.leidenuniv.nl"
+__status__ = "Development"
+
 
 class Token_Handler:
-    def __init__(self,t_type="token",srv="https://picas-lofar.grid.sara.nl:6984",uname="",pwd="",dbn=""):
-        self.t_type=t_type
-        self.Picas_User=uname
-        self.Picas_DB=dbn
-        self.Picas_Passwd=pwd
-        self.server=srv
-        self.db=self.get_db(self.Picas_User,self.Picas_Passwd,self.Picas_DB,self.server)
-        self.views={}
-        self.tokens={}
+    """
 
-    def get_db(self,uname,pwd,dbn,srv):
+    The Token_Handler class uses couchdb to create, modify and delete
+    tokens and views, to attach files, or download attachments and to 
+    easily modify fields in tokens. It's initiated with the token_type, 
+    server, username, password and name of database. 
+
+    """
+
+    def __init__(self, t_type="token", srv="https://picas-lofar.grid.sara.nl:6984", uname="", pwd="", dbn=""):
+        self.t_type = t_type
+        self.Picas_User = uname
+        self.Picas_DB = dbn
+        self.Picas_Passwd = pwd
+        self.server = srv
+        self.db = self.get_db(self.Picas_User, self.Picas_Passwd, self.Picas_DB, self.server)
+        self.views = {}
+        self.tokens = {}
+
+    def get_db(self, uname, pwd, dbn, srv):
         """Logs into the Couchdb server and returns the database requested
         """
         server = couchdb.Server(srv)
-        server.resource.credentials = (uname,pwd)
+        server.resource.credentials = (uname, pwd)
         db = server[dbn]
         return db
 
-    def create_token(self,keys={},append="",attach=[]):
-        '''Creates a token, appends string to token ID if requested and adds 
+    def create_token(self, keys={}, append="", attach=[]):
+        '''Creates a token, appends string to token ID if requested and adds
             user requested keys through the dict keys{}
-            ie t1.create_token(keys={"OBSID":"L123458","freq_res":4,"time_res":4,},append="L123458")
+            ie t1.create_token(keys = {"OBSID":"L123458","freq_res":4,"time_res":4,},append="L123458")
+            attach is [file_handle,"name of attachment"]
         '''
-        default_keys={
+        default_keys = {
             '_id': 't_'+self.t_type+"_",
             'type': self.t_type,
             'lock': 0,
@@ -43,52 +62,44 @@ class Token_Handler:
             'scrub_count': 0,
             'output': ""
         }
-        keys= dict(itertools.chain(keys.iteritems(), default_keys.iteritems()))
-        self.append_id(keys,append)
-        self.tokens[keys["_id"]]=keys
-        self.db.update([keys]) 
+        keys = dict(itertools.chain(keys.iteritems(), default_keys.iteritems()))
+        self.append_id(keys, append)
+        self.tokens[keys["_id"]] = keys
+        self.db.update([keys])
         if attach:
-            self.add_attachment(keys['_id'],attach[0],attach[1])
-        keys={}
+            self.add_attachment(keys['_id'], attach[0], attach[1])
+        return keys['_id']  # returns the token ID
 
-    def append_id(self,keys,app=""):
-        keys["_id"]+=app
-        
+    def append_id(self, keys, app=""):
+        keys["_id"] += app
 
     def get_views(self):
         """Helper function to get the current views on the database
         """
-        db_views=self.db.get("_design/"+self.t_type)
-        self.views=db_views["views"]
+        db_views = self.db.get("_design/"+self.t_type)
+        self.views = db_views["views"]
 
-
-    def delete_tokens(self,view_name="test_view",key=["",""]):
+    def delete_tokens(self, view_name="test_view", key=["", ""]):
         """Deletes tokens from view view_name
             exits if the view doesn't exist
             User can select which tokens within the view to delete
             t1.delete_tokens("todo",["OBSID","L123456"])
             t1.delete_tokens("error")
         """
-        self.get_views()
-        if view_name in self.views:
-            view=self.views[view_name]
-        else:
-            print "View Named "+view_name+" Doesn't exist"
-            return
-        v=self.db.view(self.t_type+"/"+view_name)
+        v = self.list_tokens_from_view(view_name)
         for x in v:
             document = self.db[x['key']]
-            if key[0]=="":
+            if key[0] == "":
                 pass
             else:
-                if not document[key[0]]==key[1]:
+                if not document[key[0]] == key[1]:
                     continue
             print "Deleting Token "+x['id']
             self.db.delete(document)
         #    self.tokens.pop(x['id'])
-        #TODO:Pop tokens from self
+        # TODO:Pop tokens from self
 
-    def add_view(self,v_name="test_view",cond='doc.lock > 0 && doc.done > 0 && doc.output < 0 '):
+    def add_view(self, v_name="test_view", cond='doc.lock > 0 && doc.done > 0 && doc.output < 0 '):
         """Adds a view to the db, needs a view name and a condition. Emits all tokens with
             the type of the current Token_Handler
         """
@@ -101,12 +112,12 @@ class Token_Handler:
           }
         }
         '''
-        view=ViewDefinition(self.t_type, v_name, generalViewCode %(self.t_type,cond))
-        self.views[v_name]=view
+        view = ViewDefinition(self.t_type, v_name, generalViewCode % (self.t_type, cond))
+        self.views[v_name] = view
         view.sync(self.db)
-  
+
     def add_overview_view(self):
-        overviewMapCode='''
+        overviewMapCode = '''
 function(doc) {
    if(doc.type == "%s") {
        if (doc.lock == 0 && doc.done == 0){
@@ -124,60 +135,55 @@ function(doc) {
        if(doc.lock > 0 && doc.status=='launched') {
           emit('waiting', 1);
        }
-       if(doc.lock > 0 && "starting_generic_pipeline" in doc.times) {
+       if(doc.lock > 0 && "starting_generic_pipeline" in doc.times && doc.status!='done' && doc.status!='error' && doc.status!='downloading' ) {
           emit('running', 1);
        }
    }
 }
 '''
-        overviewReduceCode='''
+        overviewReduceCode = '''
 function (key, values, rereduce) {
    return sum(values);
 }
 '''
-        overview_total_view = ViewDefinition(self.t_type, 'overview_total', overviewMapCode %(self.t_type), overviewReduceCode)
-        self.views['overview_total']=overview_total_view
-        overview_total_view.sync(self.db) 
+        overview_total_view = ViewDefinition(self.t_type, 'overview_total',
+                                             overviewMapCode % (self.t_type),
+                                             overviewReduceCode)
+        self.views['overview_total'] = overview_total_view
+        overview_total_view.sync(self.db)
 
-    def del_view(self,v_name="test_view"):
+    def del_view(self, v_name="test_view"):
         '''Deletes the view with view name from the _design/${token_type} document
             and from the token_Handler's dict of views
         '''
-        db_views=self.db.get("_design/"+self.t_type)
-        db_views["views"].pop(v_name,None)
-        self.views.pop(v_name,None)
+        db_views = self.db.get("_design/"+self.t_type)
+        db_views["views"].pop(v_name, None)
+        self.views.pop(v_name, None)
         self.db.update([db_views])
- 
+
     def remove_Error(self):
         '''Removes all tokens in the error view
         '''
-        cond="doc.lock > 0 && doc.done > 0 && doc.output > 0"
-        self.add_view(v_name="error",cond=cond)
+        cond = "doc.lock > 0 && doc.done > 0 && doc.output > 0"
+        self.add_view(v_name="error", cond=cond)
         self.delete_token("error")
 
-    def reset_tokens(self,view_name="test_view",key=["",""],del_attach=False):
+    def reset_tokens(self, view_name="test_view", key=["", ""], del_attach=False):
         """ resets all tokens in a view, optionally can reset all tokens in a view
             who have key-value pairs matched by key[0],key[1]
             t1.reset_token("error")
             t1.reset_token("error",key=["OBSID","L123456"])
             t1.reset_token("error",key=["scrub_count",6])
         """
-        if view_name in self.views:
-            view=self.views[view_name]
-        else:
-            print "View Named "+view_name+" Doesn't exist"
-            return
-        v=self.db.view(self.t_type+"/"+view_name)
-        to_update=[]
+        v = self.list_tokens_from_view(view_name)
+        to_update = []
         for x in v:
             document = self.db[x['key']]
 
-            if key[0]!="" and document[key[0]]!=key[1]: #make it not just equal
-                continue                        
-
-
+            if key[0] != "" and document[key[0]] != key[1]:  # make it not just equal
+                continue
             try:
-                document['status']='todo'
+                document['status'] = 'todo'
             except KeyError:
                 pass
             document['lock'] = 0
@@ -186,23 +192,108 @@ function (key, values, rereduce) {
             document['hostname'] = ''
             document['output'] = ''
             if del_attach:
-                    if document.has_key("_attachments"):
+                    if "_attachments" in document:
                         del document["_attachments"]
+            to_update.append(document)
+        self.db.update(to_update)
+        return (to_update)
+
+    def add_attachment(self, token, filehandle, filename="test"):
+        self.db.put_attachment(self.db[token], filehandle, filename)
+
+    def list_attachments(self, token):
+        return self.db[token]["_attachments"].keys()
+
+    def get_attachment(self, token, filename, savename=None):
+        try:
+            attach = self.db.get_attachment(token, filename).read()
+        except AttributeError:
+            print "error getting attachment"
+            return ""
+        if "/" in filename:
+            savename = filename.replace("/", "_")
+        if not savename:
+            savename = filename
+        with open(savename, 'w') as f:
+            for line in attach:
+                f.write(line)
+        return os.path.abspath(filename)
+
+    def list_tokens_from_view(self, view_name):
+        self.get_views()
+        if view_name in self.views:
+            view = self.views[view_name]
+        else:
+            print "View Named "+view_name+" Doesn't exist"
+            return
+        v = self.db.view(self.t_type+"/"+view_name)
+        return v
+
+    def set_view_to_status(self, view, status):
+        """Sets the status to all tokens in 'view' to 'status
+            eg. Set all locked tokens to error or all error tokens to todo
+        """
+        v = self.list_tokens_from_view(view)
+        to_update = []
+        for x in v:
+            document = self.db[x['key']]
+            document['status'] = str(status)
+            document['lock'] = 1
             to_update.append(document)
         self.db.update(to_update)
 
 
+class View(object):
+    def __init__(self, t_type='test', srv="https://picas-lofar.grid.sara.nl:6984", uname="", pwd="", dbn="", name="test_view"):
+        self.name = name
+        self.t_type = t_type
+        self.Picas_User = uname
+        self.Picas_DB = dbn
+        self.Picas_Passwd = pwd
+        self.server = srv
+        self.db = self.get_db(self.Picas_User, self.Picas_Passwd, self.Picas_DB, self.server)
+        self.condition = "doc.lock==0"
+        self.emit = "doc._id"
 
-    def add_attachment(self,token,filehandle,filename="test"):
-        self.db.put_attachment(self.db[token],filehandle,filename)
+    def get_db(self, uname, pwd, dbn, srv):
+        """Logs into the Couchdb server and returns the database requested
+        """
+        server = couchdb.Server(srv)
+        server.resource.credentials = (uname, pwd)
+        db = server[dbn]
+        return db
 
-    def list_attachments(self,token):
-        return self.db[token]["_attachments"].keys()
+    def set_condition(self, condition):
+        self.condition = condition
 
-    def get_attachment(self,token,filename):
-        attach=self.db.get_attachment(token,filename).read()
-        with open(filename,'w') as f:
-            for line in attach:
-                f.write(line)
-        return os.abspath(filename)
+    def set_emit(self, emit):
+        self.emit = emit
 
+    def build_Map_Code(self, mapcode=""):
+        if mapcode == "":
+            mapcode = '''function(doc) {
+   if(doc.type == "%s") {
+       if (doc.lock == 0 && doc.done == 0){
+          emit('todo', 1);
+       }
+       if(doc.lock > 0 && doc.status=='downloading') {
+          emit('downloading', 1);
+       }
+       if(doc.lock > 0 && doc.status=='done') {
+          emit('done', 1);
+       }
+       if(doc.lock > 0 && doc.status=='error') {
+          emit('error', 1);
+       }
+       if(doc.lock > 0 && doc.status=='launched') {
+          emit('waiting', 1);
+       }
+       if(doc.lock > 0 && "launched" in doc.times && doc.status!='done' && doc.status!='error' && doc.status!='downloading' ) {
+          emit('running', 1);
+       }
+   }
+}'''
+        self.mapcode = str(mapcode % (self.t_type))
+
+    def build_reduce_code(self, reduce_code=""):
+        self.reducecode = reduce_code
