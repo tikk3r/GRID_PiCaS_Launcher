06db8e41534c0eb0cff893daf45d120ca23d8546
Alexandar Mechev <apmechev@softdrive.grid.sara.nl>
2017-02-06 15:13:07 +0100

    more bugs

--- getSBX.py
+++ getSBX.py
@@ -61,7 +61,7 @@ class ExampleActor(RunActor):
         export_tok_keys('tokvar.cfg')
     
         ## Read tokvar values from token and write to bash variables if not already exist! Save attachments and export abs filename to variable
-        set_token_field(tok_id=token['_id'],fieldname='status',value='launched')
+        set_token_field(tok_id=token['_id'],fieldname='status',value='launched',p_db=os.environ['PICAS_DB'],p_usr=s.environ['PICAS_USR'],p_pwd=os.environ['PICAS_USR_PWD'])
     
         #The launched script is simply master.sh with token and picas authen stored in env vars
         #master.sh takes the variables straight from the token. 
