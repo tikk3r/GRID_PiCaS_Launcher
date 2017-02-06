b80313822588eac1c2b19de315108a7479989f46
Alexandar Mechev <apmechev@softdrive.grid.sara.nl>
2017-02-06 14:40:49 +0100

    tok_to_bash now downloads attachments (If '$' in tokvar.cfg, it saves the filename in the env variable, but not full path, since it needs to be copied to the )

--- tok_to_bash.py
+++ tok_to_bash.py
@@ -1,5 +1,6 @@
 #!/bin/env python 
 import sys,os
+import Token
 from get_token_field import get_token_field
 
 try:
@@ -18,8 +19,12 @@ def export_tok_keys(cfgfile='tokvar.cfg'):
             picas_val=get_token_field(os.environ['TOKEN'],key,db,un,pwd)
             os.environ[tokvar[key].split('$')[1]]=picas_val
         elif key=='_attachments':
-            pass
-
+            t_type=get_token_field(os.environ['TOKEN'],'type',db,un,pwd)
+            th=Token.Token_Handler(t_type=t_type,uname=un,pwd=pwd,dbn=db)
+            for att_file in tokvar['_attachments']:
+                th.get_attachment(os.environ['TOKEN'],att_file,savename=att_file) 
+                if '$' in tokvar['_attachments'][att_file]:
+                    os.environ[tokvar['_attachments'][att_file].split('$')[1]]=att_file
 
 if __name__ == '__main__':
     export_tok_keys('tokvar.cfg')
