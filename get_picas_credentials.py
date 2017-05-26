9fdd19ec7975d90642fcbaba4f8223374d362cf6
Alexandar Mechev <apmechev@loui.grid.sara.nl>
2017-05-26 13:18:27 +0200

    picas_credential internal variable is now password instead of passw and tok2bash test in the test folder

--- get_picas_credentials.py
+++ get_picas_credentials.py
@@ -14,7 +14,7 @@ class picas_cred():
                     self.get_picas_creds_from_env()
         else:
             self.user=usr
-            self.passw=pwd
+            self.password=pwd
             self.database=dbn
 
 
@@ -25,14 +25,14 @@ class picas_cred():
                 if line.startswith("user"):
                     self.user = line.split('=')[1].strip()
                 if line.startswith("password"):
-                    self.passw = line.split('=')[1].strip()
+                    self.password = line.split('=')[1].strip()
                 if line.startswith("database"):
                     self.database = line.split('=')[1].strip()
     
     def get_picas_creds_from_env(self):
         try:
             self.user=environ['PICAS_USR']
-            self.passw=environ['PICAS_USR_PWD']
+            self.password=environ['PICAS_USR_PWD']
             self.database=environ['PICAS_DB']
         except KeyError:
             print("PICAS Variable not in ENV!") 
@@ -49,7 +49,7 @@ class picas_cred():
             self.database=picas_db
             creds['PICAS_DB']=picas_db
         environ['PICAS_USR']=self.user
-        environ['PICAS_USR_PWD']=self.passw
+        environ['PICAS_USR_PWD']=self.password
         if self.database:
             environ['PICAS_DB']=self.database
         else: environ['PICAS_DB']=self.database
@@ -58,10 +58,10 @@ class picas_cred():
     def put_creds_in_file(self,pic_file="~/.picasrc"):
         with open(expanduser(pic_file),'w') as file:
             file.write("user="+str(self.user)+"\n")
-            file.write("password="+str(self.passw)+"\n")
+            file.write("password="+str(self.password)+"\n")
             file.write("database="+str(self.database)+"\n")
         chmod(expanduser(pic_file),384)
 
     def return_credentials(self):
-        return {'user':self.user, 'password':self.passw,'database':self.database}
+        return {'user':self.user, 'password':self.password,'database':self.database}
         
--- tests/test_tok_2_bash.py
+++ tests/test_tok_2_bash.py
@@ -1,4 +1,36 @@
 import unittest
+import get_picas_credentials as gpc
+from get_token_field import get_token_field
+from set_token_field import set_token_field
+import os
+
 
 class tok2bashtest(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.token='test_GRID_picastools'
+        pc=gpc.picas_cred()
+        creds=pc.return_credentials()  
+        self.usr=creds['user']
+        self.pwd=creds['password']
+        self.dbn=creds['database']
+        self.test_tokvarile='tests/test_tok_var.cfg'
+        with open(self.test_tokvarile,'w') as f:
+            f.write('string1: $STRING1'+'\n')
+            f.write('_id: $TOKEN'+'\n')
+            f.write('integer1: $INT1'+'\n')
+        set_token_field(self.token,'string1','test_string',self.dbn,self.usr,self.pwd)
+        set_token_field(self.token,'integer1',1234,self.dbn,self.usr,self.pwd)
+
+    def test_read_string(self):
+        os.environ['TOKEN']=self.token
+        from tok_to_bash import export_tok_keys
+        export_tok_keys(self.test_tokvarile,self.token)
+        self.assertTrue(os.environ['STRING1']=='test_string')
+
+    def test_read_int(self):
+        os.environ['TOKEN']=self.token
+        from tok_to_bash import export_tok_keys
+        export_tok_keys(self.test_tokvarile,self.token)
+        self.assertTrue(os.environ['INT1']=='1234')
+
