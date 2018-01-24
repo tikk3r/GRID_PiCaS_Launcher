a1822e960606373a925b71ce371abc7d22e4f41e
apmechev <apmechev@gmail.com>
2018-01-24 14:23:02 +0100

    Upgrading tok_to_bash

--- tok_to_bash.py
+++ tok_to_bash.py
@@ -2,6 +2,7 @@
 import sys,os
 import couchdb
 from get_token_field import get_token_field
+from set_token_field import set_token_field
 
 try:
     import yaml
@@ -14,7 +15,7 @@ def get_attachment(db,token, filename, savename=None):
     try:
         attach = db.get_attachment(token, filename).read()
     except AttributeError:
-        print("error getting attachment")
+        sys.stderr.write("error getting attachment")
         return ""
     if "/" in filename:
         savename = filename.replace("/", "_")
@@ -27,16 +28,25 @@ def get_attachment(db,token, filename, savename=None):
 
 
 def export_tok_keys(cfgfile='tokvar.cfg',token=None):
-    tokvar=yaml.load(open(cfgfile,'rb'))
     dbn=os.environ['PICAS_DB']
     un=os.environ['PICAS_USR']
     pwd=os.environ['PICAS_USR_PWD']
+    try:
+        tokvar=yaml.load(open(cfgfile,'rb'))
+    except:
+        set_token_field(token['_id'],'output',-2,dbn,un,pwd)
+        raise Exception("tokvar missing")
+
     server = couchdb.Server("https://picas-lofar.grid.sara.nl:6984")
     server.resource.credentials = (un, pwd)
     db = server[dbn]
     for key in tokvar:
         if isinstance(tokvar[key],str):
-            picas_val=str(get_token_field(token['_id'],key,dbn,un,pwd))
+            try:
+                picas_val=str(get_token_field(token['_id'],key,dbn,un,pwd))
+            except KeyError:
+                sys.stderr.write("WARNING: Picas Variable Missing:"+key)
+                picas_val=""
             os.environ[tokvar[key].split('$')[1]]=picas_val
         elif key=='_attachments':
             for att_file in tokvar['_attachments']:
