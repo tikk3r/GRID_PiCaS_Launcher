860f6333db2361986e0a966939e17ffec11f0507
Alexandar Mechev <apmechev@gmail.com>
2018-03-07 09:56:57 +0100

    Updating upload naming scheme for pref_cal1

--- bin/upload_results.sh
+++ bin/upload_results.sh
@@ -39,7 +39,7 @@ function upload_results_cal1(){
 
  uberftp -mkdir ${RESULTS_DIR}/${OBSID}
 
- globus-url-copy ${RUNDIR}/Output/instruments_${OBSID}_${STARTSB}.tar ${RESULTS_DIR}/${OBSID}/instruments_${OBSID}_${STARTSB}.tar  || { echo "Upload Failed"; exit 31;} # exit 31 => Upload to storage failed   
+ globus-url-copy ${RUNDIR}/Output/instruments_${OBSID}_${STARTSB}.tar ${RESULTS_DIR}/${OBSID}/instruments_${OBSID}_SB${STARTSB}.tar  || { echo "Upload Failed"; exit 31;} # exit 31 => Upload to storage failed   
 }
 
 function upload_results_cal2(){
