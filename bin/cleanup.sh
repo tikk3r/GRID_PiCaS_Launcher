5a96ba67f179a70b4f61ab79f810072fac508fe2
Alexandar Mechev <apmechev@gmail.com>
2018-03-21 16:48:16 +0100

    Adding cleanup codegit add cleanup.sh

---
+++ bin/cleanup.sh
@@ -0,0 +1,31 @@
+function cleanup(){
+if [[ "$?" != "0" ]]; then
+   echo "Problem copying final files to the Grid. Clean up and Exit now..."
+   cp log_$name logtar_$name.fa ${JOBDIR}
+   cd ${JOBDIR}
+
+   if [[ $(hostname -s) != 'loui' ]]; then
+    echo "removing RunDir"
+    rm -rf ${RUNDIR}
+   fi
+   exit 1 #exit 21=> cannot upload final files
+fi
+echo ""
+
+echo ""
+echo "copy echos to the Job home directory and clean temp files in scratch"
+cp out* ${JOBDIR}
+cp pngs.tar.gz ${JOBDIR}
+cd ${JOBDIR}
+
+if [[ $(hostname -s) != 'loui' ]]; then
+    echo "removing RunDir"
+    rm -rf ${RUNDIR}
+fi
+ls -l ${RUNDIR}
+echo ""
+echo "listing final files in Job directory"
+ls -allh $PWD
+echo ""
+du -hs $PWD
+}
