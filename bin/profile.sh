ad754ae211a83f54218876c4d65dd71c7bb7e321
Alexandar Mechev <apmechev@gmail.com>
2018-05-09 10:29:07 +0200

    preparing to add pipeline_step to profiling

--- bin/profile.sh
+++ bin/profile.sh
@@ -2,18 +2,32 @@
 
 function start_profile(){
 echo "start tCollector in dryrun mode"
-cd ${RUNDIR}/tcollector/
-mkdir logs
-mkdir run
-./tcollector.py -H spui.grid.sara.nl -p 4242  &
-TCOLL_PID=$!
+source /cvmfs/softdrive.nl/apmechev/go_packages/init.sh 
+
+echo "" > ${RUNDIR}/pipeline_step
+while /bin/true; do 
+   monitor_step
+done & 
+
+#cd ${RUNDIR}/tcollector/
+#mkdir logs
+#mkdir run
+#./tcollector.py -H spui.grid.sara.nl -p 4242  &
+########
+#telegraf --config /cvmfs/softdrive.nl/apmechev/go_packages/src/github.com/influxdata/telegraf/telegraf.conf 2>/dev/null &
+########
+COLL_PID=$!
 cd ${RUNDIR}
 
 }
 
+
 function stop_profile(){
 echo "killing tcollector"
-kill $TCOLL_PID
+kill $COLL_PID
 
+}
 
+function monitor_step(){
+export PIPELINE_STEP=$( cat ${RUNDIR}/pipeline_step )
 }
