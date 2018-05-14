f917201070036e75b96bd9c76c5867423f25e5d5
Alexandar Mechev <apmechev@gmail.com>
2018-05-14 15:27:12 +0200

    adding monitoring of pipeline step

--- bin/profile.sh
+++ bin/profile.sh
@@ -9,10 +9,7 @@ while /bin/true; do
    monitor_step
 done & 
 
-#cd ${RUNDIR}/tcollector/
-#mkdir logs
-#mkdir run
-#./tcollector.py -H spui.grid.sara.nl -p 4242  &
+
 ########
 #telegraf --config /cvmfs/softdrive.nl/apmechev/go_packages/src/github.com/influxdata/telegraf/telegraf.conf 2>/dev/null &
 ########
@@ -30,4 +27,11 @@ kill $COLL_PID
 
 function monitor_step(){
 export PIPELINE_STEP=$( cat ${RUNDIR}/pipeline_step )
+
+if [ "$PIPELINE_STEP" != "$CURR_STEP"  ]; then
+  echo Current Step is $PIPELINE_STEP; 
+  export CURR_STEP=$PIPELINE_STEP;
+fi; 
+sleep 1;
+
 }
