8251a909191fafa63395cc1027dd7ccadc90aecf
Alexandar Mechev <apmechev@gmail.com>
2018-05-24 09:37:50 +0200

    first test of telegraf

--- bin/profile.sh
+++ bin/profile.sh
@@ -5,9 +5,13 @@ echo "start tCollector in dryrun mode"
 source /cvmfs/softdrive.nl/apmechev/go_packages/init.sh 
 
 echo "" > ${RUNDIR}/pipeline_step
-while /bin/true; do 
-   monitor_step
-done & 
+mkfifo ${RUNDIR}/pipeline_fifo
+
+
+#while /bin/true; do 
+#   monitor_step
+#done  &
+
 
 
 ########
@@ -22,16 +26,28 @@ cd ${RUNDIR}
 function stop_profile(){
 echo "killing tcollector"
 kill $COLL_PID
-
 }
 
 function monitor_step(){
-export PIPELINE_STEP=$( cat ${RUNDIR}/pipeline_step )
+ export PIPELINE_STEP=$( cat ${RUNDIR}/pipeline_step )
+ 
+  if [ "$PIPELINE_STEP" != "$CURR_STEP"  ]; then
+      if [ ! -z $( echo $COLL_PID ) ]; then
+          kill $COLL_PID;
+      fi
+      launch_telegraf &
+      export COLL_PID=$!
+      export CURR_STEP=$PIPELINE_STEP;
+  fi; 
+ sleep 1;
+}
 
-if [ "$PIPELINE_STEP" != "$CURR_STEP"  ]; then
-  echo Current Step is $PIPELINE_STEP; 
-  export CURR_STEP=$PIPELINE_STEP;
-fi; 
-sleep 1;
+function monitor_loop(){
+ while /bin/true; do
+       monitor_step
+ done  
+}
 
+function launch_telegraf(){
+   telegraf --config /cvmfs/softdrive.nl/apmechev/go_packages/src/github.com/influxdata/telegraf/telegraf.conf 2>/dev/null 
 }
