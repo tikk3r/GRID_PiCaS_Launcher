95b868db161e53d5a056c624454b04bd3caa205d
Alexandar Mechev <apmechev@gmail.com>
2018-07-16 10:16:16 +0200

    short hostname doesn't match f18-01[;...]

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -8,7 +8,7 @@ cp out* ${JOBDIR}
 #cp pngs.tar.gz ${JOBDIR}
 cd ${JOBDIR}
 
-if [[ $(hostname -s) != 'f18-01.gina.sara.nl' ]]; then
+if [[ $(hostname -s) != 'f18-01' ]]; then
     echo "removing RunDir"
     rm -rf ${RUNDIR}
 fi
