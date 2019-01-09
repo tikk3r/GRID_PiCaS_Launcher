61b97920638066e46fef265481a06dce071439fe
Alexandar Mechev <apmechev@gmail.com>
2019-01-09 13:48:48 +0100

    Adding exception for cleanup for loui

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,21 +1,30 @@
-function cleanup(){
+#!/bin/bash
+
+function cleanup(){                                                                                                                               
 
 echo ""
-python  ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'cleaning up'  
+python  $"{JOBDIR}"/GRID_PiCaS_Launcher/update_token_status.py $"{PICAS_DB}" $"{PICAS_USR}" $"{PICAS_USR_PWD}" $"{TOKEN}" 'cleaning up'
 echo ""
 echo "copy logs to the Job home directory and clean temp files in scratch"
-cp out* ${JOBDIR}
-#cp pngs.tar.gz ${JOBDIR}
-cd ${JOBDIR}
+cp out* $"{JOBDIR}"
+#cp pngs.tar.gz $"{JOBDIR}"
+cd $"{JOBDIR}" || exit 4 # Exit 4-> JOBDIR doesn't exist
 
-if [[ $(hostname -s) != 'f18-01.gina.sara.nl' ]]; then
-    echo "removing RunDir"
-    rm -rf ${RUNDIR}
+if [[ $(hostname -s) == 'f18-01.gina.sara.nl' ||    $(hostname -s) == 'loui.grid.surfsara.nl' ]]; then
+    echo "Keeping RUNDIR"
+    return 0
 fi
-#ls -l ${RUNDIR}
+
+echo "removing RunDir"
+rm -rf $"{RUNDIR}"
+
+
+
+#ls -l $"{RUNDIR}""
 echo ""
 echo "listing final files in Job directory"
-ls -allh $PWD
+ls -allh $"PWD"
 echo ""
-du -hs $PWD
+du -hs $"PWD"
 }
+
