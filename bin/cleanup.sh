7a8ba472a2a9aef6318aabc42f26b71ab93cedca
Alexandar Mechev <apmechev@gmail.com>
2019-01-14 10:24:49 +0100

    fixing quotes

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -3,12 +3,11 @@
 function cleanup(){                                                                                                                               
 
 echo ""
-python  $"{JOBDIR}"/GRID_PiCaS_Launcher/update_token_status.py $"{PICAS_DB}" $"{PICAS_USR}" $"{PICAS_USR_PWD}" $"{TOKEN}" 'cleaning up'
+python  "${JOBDIR}"/GRID_PiCaS_Launcher/update_token_status.py "${PICAS_DB}" "${PICAS_USR}" "${PICAS_USR_PWD}" "${TOKEN}" 'cleaning up'
 echo ""
 echo "copy logs to the Job home directory and clean temp files in scratch"
-cp out* $"{JOBDIR}"
-#cp pngs.tar.gz $"{JOBDIR}"
-cd $"{JOBDIR}" || exit 4 # Exit 4-> JOBDIR doesn't exist
+cp out* "${JOBDIR}"
+cd "${JOBDIR}" || exit 4 # Exit 4-> JOBDIR doesn't exist
 
 if [[ $(hostname ) == 'f18-01.gina.sara.nl' ||    $(hostname) == 'loui.grid.surfsara.nl' ]]; then
     echo "Keeping RUNDIR"
@@ -16,15 +15,15 @@ if [[ $(hostname ) == 'f18-01.gina.sara.nl' ||    $(hostname) == 'loui.grid.surf
 fi
 
 echo "removing RunDir"
-rm -rf $"{RUNDIR}"
+rm -rf "${RUNDIR}"
 
 
 
-#ls -l $"{RUNDIR}""
+
 echo ""
 echo "listing final files in Job directory"
-ls -allh $"PWD"
+ls -allh "$PWD"
 echo ""
-du -hs $"PWD"
+du -hs "$PWD"
 }
 
