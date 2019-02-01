27e022347a38f50ad8c150137a53d47cd99dfa10
Alexandar Mechev <apmechev@gmail.com>
2019-02-01 14:04:21 +0100

    workaround over the ImportError and fix of sys.argv and fix of picas_cred

--- GRID_PiCaS_Launcher/get_token_field.py
+++ GRID_PiCaS_Launcher/get_token_field.py
@@ -11,14 +11,14 @@ def get_token_field(tok_id,fieldname,p_db,p_usr,p_pwd):
     return  token[fieldname]
 
 
-if __name__ == '__main__':
-    if len(sys.argv==5):
+if __name__ == '__main__': 
+    if len(sys.argv)==6:
         value=get_token_field(sys.argv[4],sys.argv[5],sys.argv[1],sys.argv[2],sys.argv[3])
     else:
         pc = picas_cred()
-        db = picas_cred.database
-        usr = picas_cred.user
-        passw = picas_cred.password
+        dbn = pc.database
+        usr = pc.user
+        passw = pc.password
         value=get_token_field(sys.argv[-2],sys.argv[-1],dbn,usr,passw)
     print(value)
  
