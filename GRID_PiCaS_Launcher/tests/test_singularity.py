10b4305696f1e9f013a69aa57d4ccfe8ae7d9daa
Alexandar Mechev <apmechev@gmail.com>
2019-02-05 14:33:25 +0100

    covering two exeptions

--- GRID_PiCaS_Launcher/tests/test_singularity.py
+++ GRID_PiCaS_Launcher/tests/test_singularity.py
@@ -10,7 +10,7 @@ DUMMY_CONFIG = BASE_DIR+"/tests/cal_pref3_v1.0.json"
 #TODO: confirm that the branch and commit are correct using internal function
 class testsingularity(unittest.TestCase):
 
-    def test_put_env(self):
+    def test_all(self):
         config = json.load(open(DUMMY_CONFIG))
         put_variables_in_env(config)
         self.assertTrue(os.environ['SIMG'] == config['container']['singularity']['SIMG'])
@@ -21,9 +21,20 @@ class testsingularity(unittest.TestCase):
         put_variables_in_env(config['container']['singularity'])
         self.assertTrue(os.environ['SIMG'] == config['container']['singularity']['SIMG'])
         self.assertTrue(os.environ['SIMG_COMMIT'] == config['container']['singularity']['SIMG_COMMIT'])
-
-    def test_dl_shub_env(self):
-        config = json.load(open(DUMMY_CONFIG))
-        put_variables_in_env(config)
+        out = download_singularity_from_env()
+        self.assertTrue('tikk3r-lofar-grid-hpccloud-master-lofar.simg' in out) 
+        del os.environ['SIMG_COMMIT']
         out = download_singularity_from_env()
         self.assertTrue('tikk3r-lofar-grid-hpccloud-master-lofar.simg' in out)
+        del os.environ['SIMG']
+        try:
+            download_singularity_from_env()
+        except RuntimeError as e:
+            self.assertTrue(str(e) == "NO $SIMG in Environment!")
+
+    def test_badJson(self):
+        badjson={}
+        try:
+            put_variables_in_env(badjson)
+        except RuntimeError as e:
+            self.assertTrue(str(e) == "Could not find SIMG in {}")
