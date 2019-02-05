c2ae946f0ddb575caf6f8c88ef03584834712786
Alexandar Mechev <apmechev@gmail.com>
2019-02-05 13:42:52 +0100

    singularity.py has an entry point and properly returns the image path

--- GRID_PiCaS_Launcher/singularity.py
+++ GRID_PiCaS_Launcher/singularity.py
@@ -2,10 +2,22 @@ import subprocess
 import os
 import hashlib
 import warnings
+import json
+import pdb
 
-def parse_singularity_link():
+def download_singularity_from_env():
     if not 'SIMG' in os.environ.keys():
         raise RuntimeError("NO $SIMG in Environment!")
+    simg_url = os.environ['SIMG']
+    simg_commit = os.environ.get('SIMG_COMMIT', None)
+    return parse_singularity_link(simg_url, simg_commit)
+
+def parse_singularity_link(simg_url, simg_commit=None):
+    if simg_url.split("://")[0] == 'shub':
+        return pull_image_from_shub(simg_url, simg_commit)
+    if simg_url.split("://")[0] == 'gsiftp':
+        return download_simg_from_gsiftp(simg_url)
+    print("Unknown image location {0}".format(simg_url))
 
 def download_simg_from_gsiftp(simg_link):
     img_name = simg_link.split('/')[-1]
@@ -14,11 +26,12 @@ def download_simg_from_gsiftp(simg_link):
                            stderr=subprocess.PIPE)
     out,err = _dl.communicate()
     if not out and not err:
-
-        return
+        return img_name
     else:
         print("Error downloading image:{1}".format(err))
-    
+
+
+
 def pull_image_from_shub(shub_link,commit=None):
     print("Pulling image {0} with commit {1}".format(shub_link,commit))
     if not commit:
@@ -44,11 +57,25 @@ def pull_image_from_shub(shub_link,commit=None):
                 if img_hash == commit:
                     return img_path
                 else:
-                    warnings.warn("!!!!!!!!!!!!!\n !!!!!!!!!!!!!!!!\n The image commit and the hash on singularityhub are not the same!")
+                    warnings.warn("\n!!!!!!!!!!!!!\n !!!!!!!!!!!!!!!!\n The image commit and the hash on singularityhub are not the same!\n The pulled image has a hash of {0} instead of {1}".format(img_hash, commit))
                     return img_path
             else:
                 raise RuntimeError("Tried to download image to {0} but now it isn't there!".format(img_path))
 
+
+def put_variables_in_env(json_payload):
+    """Takes a json payload from a token and puts the variables in the environment"""
+    if 'container' in json_payload.keys():
+        json_payload = json_payload['container']
+    if 'singularity' in json_payload.keys():
+        json_payload = json_payload['singularity']
+    if 'SIMG' in json_payload.keys():
+        payload = json_payload
+    else:
+        raise RuntimeError("Could not find SIMG in {0}".format(json_payload))
+    os.environ['SIMG'] = payload['SIMG']
+    if "SIMG_COMMIT" in payload.keys():
+        os.environ["SIMG_COMMIT"] = payload["SIMG_COMMIT"]
 
 def get_image_file_hash(image_path):
     '''get_image_hash will return an md5 hash of the file based on a criteria level.
@@ -61,3 +88,6 @@ def get_image_file_hash(image_path):
         for chunk in iter(lambda: f.read(4096), b""):
             hasher.update(chunk)
     return hasher.hexdigest()
+
+if __name__ == "__main__":
+    download_singularity_from_env()
