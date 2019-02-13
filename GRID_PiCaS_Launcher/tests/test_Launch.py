ed04250b4fe8f7d7ee6e8a169f91be643f5caa3b
Alexandar Mechev <apmechev@gmail.com>
2019-02-13 17:23:23 +0100

    Strip full path of SIMG when comparing in testLaunch.py

--- GRID_PiCaS_Launcher/tests/test_Launch.py
+++ GRID_PiCaS_Launcher/tests/test_Launch.py
@@ -155,7 +155,7 @@ class Launchtest(unittest.TestCase):
         self.Ex.user = self.usr
         self.Ex.password = self.pwd
         self.Ex.run() 
-        self.assertEquals(os.environ['SIMG'],'tikk3r-lofar-grid-hpccloud-master-lofar.simg')
+        self.assertEquals(os.environ['SIMG'].split('/')[-1],'tikk3r-lofar-grid-hpccloud-master-lofar.simg')
         for att in self.db[self.token]['_attachments']:
             if "png" in att:
                 return
